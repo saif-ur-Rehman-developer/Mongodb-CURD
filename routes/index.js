1cce1bf8c2028081703bbf5371597e3490d8ab71
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-03-14 17:14:33 +0500

    Adding Some Documentation and Adding Update Logic

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,29 @@ var model = require('./users')
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Saif' });
 });
+
+//How to create data
+
 router.get('/blog',async (req,res)=>{
 const NewArticle1=  await model.create({
   Title: "Dominance of React JS",
   Discription: "this is the power of js in world",
-  Author: "Saif Ali" 
+  Author: "Saif" 
 })
 res.send(NewArticle1)
 console.log("check");
 })
+
+//How to read data
+
+
 router.get("/check", async(rq,res)=>{
   const Checking = await model.find()
   res.send(Checking)
 })
+
+//How to delete data
+
 router.get("/delete",async(req,res)=>{
   const Delete = await model.findOneAndDelete({
     Author:"Saif Ali"
@@ -25,5 +35,37 @@ router.get("/delete",async(req,res)=>{
   res.send(Delete)
 })
 
+//How to update data
+
+router.get("/update",async(req,res)=>{
+const update = await model.updateOne({
+  "_id": "65f17bccf2c23c59d45d5522"
+  
+},{
+$set:{
+Author:`Muhammad Imran`
+}}
+)
+  res.send(update)
+})
+
+// How wo can search in Mongodb with case sensitive
+
+router.get('/search',async (req,res)=>{
+  
+  
+  //if youu can search in db with that you cannot search properly 
+  //becuse of words sensitivty 
+  // let caseSensitive= new RegExp('Name','Flags')
+  //your author start form ^ and end from $
+ 
+ 
+  let caseSensitive= new RegExp('^Saif$','i')
+  const searching = await model.find({
+    Author:caseSensitive
+  })
+  res.send(searching)
+
+})
 
 module.exports = router;
